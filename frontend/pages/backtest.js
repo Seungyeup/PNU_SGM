998f9115b75491ead496e83faf01aa61ef4ee255
jeonghye-choi <ppllhm.0@gmail.com>
2020-08-16 17:10:25 +0900

    Seperate components in backtest page
    백테스팅 페이지 컴포넌트 세부 분리
    issue: #6

--- frontend/pages/backtest.js
+++ frontend/pages/backtest.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import DashboardLayout from '../components/layout/DashboardLayout';
 import Filter from '../components/backtest/Filter';
+import Items from '../components/backtest/Items';
+import Result from '../components/backtest/Result';
+import MonthGraph from '../components/backtest/MonthGraph';
+import OddsGraph from '../components/backtest/OddsGraph';
+import MainGraph from '../components/backtest/MainGraph';
 
 const BackTest = () => {
 
@@ -10,13 +15,13 @@ const BackTest = () => {
                 {/* 결과section */}
                 <section>
                     <div className="result-firstLine">
-                        <article className="items">종목(items)</article>
-                        <article className="result">결과(result)</article>
-                        <article className="subGraph1">graph1(월간 수익률)</article>
-                        <article className="subGraph2">graph2(승률)</article>
+                        <article className="items"><Items/></article>
+                        <article className="result"><Result/></article>
+                        <article className="subGraph1"><MonthGraph/></article>
+                        <article className="subGraph2"><OddsGraph/></article>
                     </div>
                     <div>
-                        <article className="mainGraph">메인그래프</article>
+                        <article className="mainGraph"><MainGraph/></article>
                     </div>
                     
                 </section>
